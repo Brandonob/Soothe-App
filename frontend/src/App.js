f64b7b6f0dd3951ebba9082129ec8852461c24d4
bsihame <sihamebazi@pursuit.org>
2020-06-20 17:19:11 -0400

    2nd update to clock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-// import Clock from './features/clock/Clock'
 import SetAlarmForm from './features/alarms/SetAlarmForm'
-import UpdateClock from './features/clock/UpdateClock'
+import Clock from './features/clock/Clock'
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Clock /> */}
-      <UpdateClock/>
+      <Clock/>
       <SetAlarmForm/>
     </div>
   );
